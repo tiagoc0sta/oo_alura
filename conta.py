95a29a7955eb80ad142c9cba093db4e17f1c3f4f
Tiago Costa <tiagodpv@gmail.com>
2023-07-30 12:01:21 -0400

    f

---
+++ conta.py
@@ -0,0 +1,22 @@
+class Conta:
+
+    def __init__(self, numero, titular, saldo, limite):
+        print("Construindo objeto... {}".format(self))
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+
+    def extrato(self):
+        print("Saldo de {} do titular {}".format(self.saldo, self.titular))
+
+    def deposita(self, valor):
+        self.__saldo += valor
+
+    def saca(self, valor):
+        self.__saldo -= valor
+
+    def transfere(self, valor, origem, destino):
+        origem.saca(valor)
+        destino.deposita(valor)
